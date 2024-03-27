b1032773651954b5fd92f947d156648af8ce2046
Colton Hye <hyercolt@msu.edu>
2024-03-27 16:11:44 -0400

    t

--- react-v2/src/App.js
+++ react-v2/src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MagicButton() {
+  return (
+    <div>
+      <h3>This is a magic button</h3>
+    <button>Magic</button>
+    </div>
+    
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          test
-        </a>
+        
+        <MagicButton />
       </header>
     </div>
   );

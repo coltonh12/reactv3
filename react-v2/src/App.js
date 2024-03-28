fadb589c624ef6b7a1354fe3717c7c2d107ee326
Colton Hye <hyercolt@msu.edu>
2024-03-28 11:44:20 -0400

    test

--- react-v2/src/App.js
+++ react-v2/src/App.js
@@ -5,8 +5,28 @@ const book = {
   title: 'harry potter and the chamber of secrets',
   author: 'J.k Rowling',
   published: '2007',
+  image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cavalierhousebooks.com%2Fbook%2F9780756903169&psig=AOvVaw0A_7TV6bXzyi3nRJXeWilm&ust=1711726846668000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCOiru8ill4UDFQAAAAAdAAAAABAD',
+  width: '264',
+  height: '378'
 };
 
+function Bookshelf() {
+  return (
+    <div>
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      <img
+      className="bookCover"
+      src={book.image}
+      alt={book.title + 'cover'}
+      style={{
+        width: book.width,
+        height: book.height
+      }}
+      />
+    </div>
+  );
+}
 
 function MagicButton() {
   return (

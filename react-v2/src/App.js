b7443a0d6357e8d77b927d271b91c824f5cf2287
Colton Hye <hyercolt@msu.edu>
2024-03-28 11:54:47 -0400

    t

--- react-v2/src/App.js
+++ react-v2/src/App.js
@@ -5,7 +5,7 @@ const book = {
   title: 'harry potter and the chamber of secrets',
   author: 'J.k Rowling',
   published: '2007',
-  image: 'https://en.wikipedia.org/wiki/File:Harry_Potter_and_the_Chamber_of_Secrets.jpg',
+  image: 'https://upload.wikimedia.org/wikipedia/en/5/5c/Harry_Potter_and_the_Chamber_of_Secrets.jpg',
   width: '264',
   height: '378'
 };
